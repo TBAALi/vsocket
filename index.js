566d18e32df54b4989fd9b2d7ea6778148cbec30
Lalo Dev <lalo.kob@gmail.com>
2021-02-12 15:37:20 -0600

    se modifica el puerto del socket

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-http.listen(45174, () => { console.log('listening on *:45174'); });
+http.listen(7171, () => { console.log('listening on *:7171'); });
+// http.listen(45174, () => { console.log('listening on *:45174'); });
 
 const counters = io.of('/counters');
 
